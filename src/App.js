19e8fd7646d39404e943fc0a9e50472dcbcd9ba8
edithadrien <81303044+edithadrien@users.noreply.github.com>
2023-04-11 10:57:40 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import {v4 as uuidv4} from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
-import Employees from './pages/Employees'
+import Employees from './pages/Employees';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Customers from './pages/Customers';
 
 function App() {
  
   return (
     <Header>
-      <Employees />
+      <BrowserRouter>
+          <Routes>
+            <Route path='/employees' element={<Employees />}/>
+            <Route path='/customers' element={<Customers />}/>
+          </Routes>
+      </BrowserRouter>
     </Header>
   );
 }
