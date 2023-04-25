43d2fa50ff6c7967dcaab05df8fca1dc91a7f51c
edithadrien <81303044+edithadrien@users.noreply.github.com>
2023-04-25 13:33:11 +0300

    redirect with useNavigate, style the dictionary

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Customers from './pages/Customers';
 import Dictionary from './pages/Dictionary';
 import Definition from './pages/Definition';
+import NotFound from './components/NotFound';
 
 function App() {
  
@@ -21,16 +22,18 @@ function App() {
           <Routes> 
             <Route path='/employees' element={<Employees />}/>
             <Route path='/dictionary' element={<Dictionary />}/>
-            <Route path='/definition' element={<Definition />}/>
+            
             <Route 
             path='/definition/:search' 
             element={<Definition />}
             />
             <Route path='/customers' element={<Customers />}/>
+            <Route path="/404" element={<NotFound />}/>
           </Routes>
         </Header>
       </BrowserRouter>
   );
 }
+
 
 export default App;
